b210469118942816f5f04458de14555fd5473619
andreyfeelman <andreyfeelman@gmail.com>
2017-12-23 21:35:58 +0200

    Д/з 1

---
+++ Summer.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Summer {
+
+    public static void main(String[] args) {
+        System.out.println("Как я провел лето 2017");//good, but it is not exactly
+
+    }
+}
